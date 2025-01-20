da04353621ee7b3500b9d47e59a09d3c98cf8325
dnwldla <94666277+dnwldla@users.noreply.github.com>
2025-01-20 13:13:32 +0900

    [Gold IV] Title: 치즈, Time: 252 ms, Memory: 23604 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.java
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -1,66 +1,69 @@
 import java.util.*;
 
 public class Main {
-    static int[][] a = new int[104][104];
-    static boolean[][] visited = new boolean[104][104];
-    static int[] dy = {-1, 0, 1, 0};
-    static int[] dx = {0, 1, 0, -1};
-    static int n, m, cnt, cnt2;
-    static List<int[]> v = new ArrayList<>();
-
-    static void go(int y, int x) {
-        visited[y][x] = true;
-        if (a[y][x] == 1) {
-            v.add(new int[]{y, x});
-            return;
-        }
-        for (int i = 0; i < 4; i++) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if (ny < 0 || ny >= n || nx < 0 || nx >= m || visited[ny][nx]) continue;
-            go(ny, nx);
-        }
-    }
-
+    static int[][] graph;
+    static boolean[][] visited;
+    static int N,M;
+    static int ret1,ret2;
+    static int[] dx={0,0,-1,1};
+    static int[] dy={1,-1,0,0};
+    static List<int[]> cheese=new ArrayList<>();
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
+        Scanner sc=new Scanner(System.in);
+        N=sc.nextInt();
+        M=sc.nextInt();
+        graph=new int[N][M];
+        visited=new boolean[N][M];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                a[i][j] = sc.nextInt();
+        for (int i=0;i<N;i++){
+            for (int j=0;j<M;j++){
+                graph[i][j]=sc.nextInt();
             }
         }
 
-        while (true) {
-            // Reset visited array
-            for (int i = 0; i < 104; i++) {
-                Arrays.fill(visited[i], false);
-            }
-
-            v.clear();
-            go(0, 0);
-
-            cnt2 = v.size();
-            for (int[] b : v) {
-                a[b[0]][b[1]] = 0;
-            }
-
-            boolean flag = false;
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    if (a[i][j] != 0) flag = true;
+        while (true){
+            boolean allZero=true;
+            //모두 0이면 break
+            for (int i=0;i<N;i++){
+                for (int j=0;j<M;j++){
+                    if (graph[i][j]==1) allZero=false;
                 }
             }
+            if (allZero) break;
 
-            cnt++;
-            if (!flag) break;
+            cheese.clear();
+            //0인 부분에서 dfs
+            for (int i=0;i< visited.length;i++){
+                Arrays.fill(visited[i],false);
+            }
+            go(0,0);
+
+            for (int[] c:cheese){
+                graph[c[0]][c[1]]=0;
+            }
+            //시간을 더한다
+            ret1++;
+            //남아있는 치즈개수를 구한다
+            ret2=cheese.size();
         }
 
-        System.out.println(cnt);
-        System.out.println(cnt2);
+       System.out.println(ret1);
+        System.out.println(ret2);
+    }
 
-        sc.close();
+    static void go(int i,int j){
+       visited[i][j]=true;
+        if (graph[i][j]==1){
+            cheese.add(new int[]{i,j});
+            return;
+        }
+        for (int k=0;k<4;k++){
+            int nx=i+dx[k];
+            int ny=j+dy[k];
+
+            if (nx<0 || nx>=N || ny<0 || ny>=M || visited[nx][ny]) continue;
+            go(nx,ny);
+        }
+
     }
 }

6e7676e35c2137d1230c47dee3bae8debc241cdb
dnwldla <94666277+dnwldla@users.noreply.github.com>
2025-06-27 10:59:47 +0900

    [Gold III] Title: 불!, Time: 676 ms, Memory: 69700 KB -BaekjoonHub

--- Java/백준/Gold/4179. 불！/불！.java
+++ Java/백준/Gold/4179. 불！/불！.java
@@ -1,99 +1,131 @@
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class Main {
-    static int R, C;
-    static char[][] map;
-    static boolean[][] fireVisited;
-    static boolean[][] locVisited;
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {1, -1, 0, 0};
+    static boolean flag;
+    static int R,C,ret;
+    static char[][] map = new char[1001][1001];
+    static boolean[][] visited = new boolean[1001][1001];
+    static class Pair{
+        int x;int y;
 
-    static boolean outOfRange(int x, int y) {
-        return x < 0 || x >= R || y < 0 || y >= C;
+        public Pair(int x,int y){
+            this.x=x;
+            this.y=y;
+        }
+
+        public int getX(){
+            return x;
+        }
+
+        public int getY(){
+            return y;
+        }
+
+        boolean isEnd() {
+            if (x==0 || x==R-1) return true;
+            if (y==0 || y==C-1) return true;
+            return false;
+        }
     }
 
-    static boolean isWall(int x, int y) {
-        return map[x][y] == '#';
+    static boolean outOfRange(int x,int y){
+        if (x<0 || x>=R || y<0 || y>=C) return true;
+        return false;
     }
 
-    static boolean isEdge(int x, int y) {
-        return x == 0 || y == 0 || x == R - 1 || y == C - 1;
+    static boolean isWall(int x,int y){
+        if (map[x][y] == '#') return true;
+        return false;
+    }
+
+    static  boolean isEnd(int x,int y) {
+        if (x==0 || x==R-1) return true;
+        if (y==0 || y==C-1) return true;
+        return false;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        R = sc.nextInt();
-        C = sc.nextInt();
-        map = new char[R][C];
-        fireVisited = new boolean[R][C];
-        locVisited = new boolean[R][C];
+        R = sc.nextInt();C=sc.nextInt();
+        int[] dx = {0,0,-1,1};
+        int[] dy = {1,-1,0,0};
 
-        Queue<int[]> fireQ = new LinkedList<>();
-        Queue<int[]> locQ = new LinkedList<>();
+        Queue<Pair> fireQ=new LinkedList<>();
+        Queue<Pair> locQ=new LinkedList<>();
 
         for (int i = 0; i < R; i++) {
-            String line = sc.next();
+            String s=sc.next();
             for (int j = 0; j < C; j++) {
-                map[i][j] = line.charAt(j);
-                if (map[i][j] == 'J') {
-                    locQ.add(new int[]{i, j});
-                    locVisited[i][j] = true;
-                } else if (map[i][j] == 'F') {
-                    fireQ.add(new int[]{i, j});
-                    fireVisited[i][j] = true;
-                }
+                map[i][j]=s.charAt(j);
+
+                if (map[i][j]=='J') {locQ.add(new Pair(i,j)); visited[i][j]=true;}
+                else if (map[i][j]=='F') {fireQ.add(new Pair(i,j)); visited[i][j]=true;}
+
             }
         }
 
-        // 시작부터 가장자리면 탈출 성공
-        int[] start = locQ.peek();
-        if (isEdge(start[0], start[1])) {
+        if (locQ.peek().isEnd()){
             System.out.println(1);
             return;
         }
+        ret=1;
+        boolean impossible=false;
+        while (true){
+            int fireSize=fireQ.size();
+            while (fireSize>0){
+                Pair fire=fireQ.poll();
+                fireSize--;
+                //상하좌우 벽 제외하고 불뿌리기
+                for (int i=0;i<4;i++){
+                    int nx=fire.getX()+dx[i];
+                    int ny=fire.getY()+dy[i];
 
-        int time = 0;
-        while (!locQ.isEmpty()) {
-            time++;
+                    if (outOfRange(nx,ny)) continue;
+                    if (isWall(nx,ny)) continue;
+                    if (visited[nx][ny]) continue;
 
-            // 불 먼저 확산
-            int fireSize = fireQ.size();
-            while (fireSize-- > 0) {
-                int[] fire = fireQ.poll();
-                for (int d = 0; d < 4; d++) {
-                    int nx = fire[0] + dx[d];
-                    int ny = fire[1] + dy[d];
-
-                    if (outOfRange(nx, ny)) continue;
-                    if (isWall(nx, ny) || fireVisited[nx][ny]) continue;
-
-                    fireVisited[nx][ny] = true;
-                    fireQ.add(new int[]{nx, ny});
+                    fireQ.add(new Pair(nx,ny));
+                    visited[nx][ny]=true;
+                    map[nx][ny]='F';
                 }
             }
 
-            // 지훈 이동
-            int locSize = locQ.size();
-            while (locSize-- > 0) {
-                int[] loc = locQ.poll();
-                for (int d = 0; d < 4; d++) {
-                    int nx = loc[0] + dx[d];
-                    int ny = loc[1] + dy[d];
+            int locSize=locQ.size();
+            //현 위치에서 이동가능한 곳 판별: 가장자리면 break, else push
+            while (locSize>0){
+                Pair loc=locQ.poll();
+                locSize--;
 
-                    if (outOfRange(nx, ny)) continue;
-                    if (isWall(nx, ny) || locVisited[nx][ny] || fireVisited[nx][ny]) continue;
+                for (int i=0;i<4;i++){
+                    int nx=loc.getX()+dx[i];
+                    int ny=loc.getY()+dy[i];
 
-                    if (isEdge(nx, ny)) {
-                        System.out.println(time + 1);
-                        return;
+                    if (outOfRange(nx,ny)) continue;
+                    if (visited[nx][ny]) continue;
+                    if (isWall(nx,ny)) continue;
+                    if (map[nx][ny]=='F') continue;
+
+                    if (isEnd(nx,ny)){
+                        flag=true;
+                        break;
                     }
+                    locQ.add(new Pair(nx,ny));
+                    visited[nx][ny]=true;
 
-                    locVisited[nx][ny] = true;
-                    locQ.add(new int[]{nx, ny});
                 }
             }
+            if (flag) break;
+            if (locQ.isEmpty()){
+                impossible=true;
+                break;
+            }
+            ret++;
         }
 
-        System.out.println("IMPOSSIBLE");
+        if (impossible) System.out.println("IMPOSSIBLE");
+        else System.out.println(ret+1);
+
     }
 }

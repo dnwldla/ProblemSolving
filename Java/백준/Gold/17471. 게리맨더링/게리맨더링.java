f890ff584a108c14dd2bcfacd893df8d3691a081
dnwldla <94666277+dnwldla@users.noreply.github.com>
2025-07-02 15:07:32 +0900

    [Gold III] Title: 게리맨더링, Time: 184 ms, Memory: 18776 KB -BaekjoonHub

--- Java/백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ Java/백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -1,96 +1,20 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    static int INF=Integer.MAX_VALUE;
-    static int ret=INF;
+    static final int INF = Integer.MAX_VALUE;
 
-    //연결고리를 찾는다
-    static void checkList(int cur,int flag,int[]a){
-        for (Integer next:graph[cur]){
-            if (a[next]!=flag) continue;
-            if (cyclecheck[next]) continue;
-            cyclecheck[next] = true;
-            checkList(next,flag,a);
-        }
-    }
-
-
-    static int check(){
-        List<Integer> A=new ArrayList<>();
-        List<Integer> B=new ArrayList<>();
-
-        for (int i=1;i<=N;i++) cyclecheck[i]=false;
-
-        int[] a=new int[N+1]; //어느 지점인지 확인하는 함수
-        for (int i=1;i<=N;i++){
-            if (visited[i]){
-                A.add(i);
-                a[i]=1;
-            }
-            else{
-                B.add(i);
-                a[i]=0;
-            }
-        }
-        if (A.isEmpty() || B.isEmpty()){
-            return INF;
-        }
-        cyclecheck[A.get(0)]=true;
-        int aCnt=0,n1=0,n2=0;
-        checkList(A.get(0),1,a);
-        for (int i=1;i<=N;i++){
-            if (cyclecheck[i]){
-                aCnt+=person[i];
-                n1++;
-            }
-        }
-        if (n1!=A.size()){
-            return INF;
-        }
-
-        int bCnt=0;
-        for (int i=1;i<=N;i++){
-            cyclecheck[i]=false;
-        }
-        cyclecheck[B.get(0)]=true;
-        checkList(B.get(0),0,a);
-
-        for (int i=1;i<=N;i++){
-            if (cyclecheck[i]){
-                bCnt+=person[i];
-                n2++;
-            }
-        }
-
-        if (n2!=B.size()) return INF;
-
-        return Math.abs(aCnt-bCnt);
-
-    }
-
-    static void go(int cur){
-        ret=Math.min(ret,check());
-//        for (int i=1;i<=N;i++){
-//            if (visited[i]) System.out.print(i+" ");
-//        }
-//        System.out.printf("&&%d\n", ret);
-
-        for (int i=cur+1;i<=N;i++){
-            if (!visited[i]){
-                visited[i]=true;
-                go(i);
-                visited[i]=false;
-            }
-        }
-    }
     static int N;
     static int[] person=new int[11];
     static List<Integer>[] graph=new List[11];
+    static int[] comp=new int[11];
     static boolean[] visited=new boolean[11];
-    static boolean[] cyclecheck=new boolean[11];
+    static int result = INF;
     public static void main(String[] args) {
+
+        //입력
         Scanner sc=new Scanner(System.in);
         N=sc.nextInt();
         for (int i=1;i<=N;i++){
@@ -107,15 +31,54 @@ public class Main {
             }
         }
 
-        //구역 나누기
+        for (int i=1;i<(1<<N)-1;i++){
+            int idx1=-1,idx2=-1;
+            Arrays.fill(comp,0);
+            Arrays.fill(visited,false);
+            for (int j=0;j<N;j++){
+                //구역 1이라면
+                if ((i&(1<<j))>0){
+                    idx1=j+1;
+                    comp[j+1]=1;
+                }else{
+                    idx2=j+1;
+                }
+            }
 
-        for (int i=1;i<=N;i++){
-            visited[i]=true;
-            go(i);
-            visited[i]=false;
+            Pair p1=go(idx1,1);
+            Pair p2=go(idx2,0);
+            
+            if (p1.cnt+p2.cnt==N){
+                result=Math.min(result,Math.abs(p1.people-p2.people));
+            }
         }
-        if (ret==INF) System.out.println(-1);
-        else System.out.println(ret);
 
+        if (result==INF) System.out.println(-1);
+        else System.out.println(result);
+    }
+
+    static Pair go(int idx,int flag){
+        Pair pair=new Pair(1,person[idx]);
+        visited[idx]=true;
+
+        for (int there:graph[idx]){
+            if (comp[there]!=flag) continue;
+            if (visited[there]) continue;
+
+            Pair future=go(there,flag);
+            pair.people+=future.people;
+            pair.cnt+=future.cnt;
+        }
+        return pair;
+    }
+
+    static class Pair{
+        int cnt;
+        int people;
+
+        public Pair(int cnt,int people){
+            this.cnt=cnt;
+            this.people=people;
+        }
     }
 }

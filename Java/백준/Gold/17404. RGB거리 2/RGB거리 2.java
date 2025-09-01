8d8598c9086540468358b64ec0eb84efc7c23473
dnwldla <94666277+dnwldla@users.noreply.github.com>
2025-09-01 14:15:21 +0900

    [Gold IV] Title: RGB거리 2, Time: 236 ms, Memory: 22320 KB -BaekjoonHub

--- Java/백준/Gold/17404. RGB거리 2/RGB거리 2.java
+++ Java/백준/Gold/17404. RGB거리 2/RGB거리 2.java
@@ -1,7 +1,10 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class Main {
-    static int N;
+    static int N,ans;
+
     public static void main(String[] args) {
+        ans=Integer.MAX_VALUE;
         Scanner sc=new Scanner(System.in);
         N=sc.nextInt();
         int[][] dist=new int[N][3];
@@ -11,33 +14,30 @@ public class Main {
                 dist[i][j]=sc.nextInt();
             }
         }
-        int ans=1000*1000+1;
-        for (int k=0;k<=2;k++){
 
-            for (int j=0;j<=2;j++){ //첫번째
-                if (j==k) dp[0][j]=dist[0][j];
-                else dp[0][j]=1000*1000+1;
+        for (int j=0;j<3;j++){ //하나씩 고정 //r,g,b
+
+            for (int i=0;i<3;i++){
+                if (j==i) dp[0][i]=dist[0][j];
+                else dp[0][i]=1001;
             }
 
-            for (int i=1;i<N;i++){
-                dp[i][0]=Math.min(dp[i-1][2],dp[i-1][1])+dist[i][0];
+            for (int i=1;i<N;i++){ //2번째부터 N-1번째 집까지
+                dp[i][0]=Math.min(dp[i-1][1],dp[i-1][2])+dist[i][0];
                 dp[i][1]=Math.min(dp[i-1][0],dp[i-1][2])+dist[i][1];
-                dp[i][2]=Math.min(dp[i-1][0],dp[i-1][1])+dist[i][2];
-            }
+                dp[i][2]=Math.min(dp[i-1][1],dp[i-1][0])+dist[i][2];
 
-            for (int j=0;j<=2;j++){
-                if (j==k) continue;
-                ans=Math.min(ans,dp[N-1][j]);
 
             }
 
+
+            for (int i=0;i<3;i++){ //처음과 끝이 같은 색이라면 continue
+                if (j==i) continue;
+                ans=Math.min(ans,dp[N-1][i]);
+            }
         }
 
         System.out.println(ans);
-
-
-
-
 
     }
 }

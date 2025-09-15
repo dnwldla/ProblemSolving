6846b0bb1cc3f05808df36f97fbb8b6383545ea7
dnwldla <94666277+dnwldla@users.noreply.github.com>
2025-09-15 13:50:46 +0900

    [Gold V] Title: 치킨 배달, Time: 172 ms, Memory: 19456 KB -BaekjoonHub

--- Java/백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ Java/백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,54 +1,73 @@
 import java.util.*;
+import java.io.*;
 
-public class Main {
-    static int N,M;
-    static List<int[]> homes=new ArrayList<>();
+public class Main{
+    static class Pair{
+        int x,y;
+
+        Pair(int x,int y){
+            this.x=x;
+            this.y=y;
+        }
+    }
+    static int N,M,ret;
     static List<int[]> chicken=new ArrayList<>();
-    static boolean[] visited;
-    static int ans=Integer.MAX_VALUE;
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+    static List<int[]> house=new ArrayList<>();
+    static Pair[] ar;
+    static boolean[][] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N=sc.nextInt();
-        M=sc.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());        
+        ar=new Pair[N*N+1];
+        visited=new boolean[N+1][N+1];
+        ret=Integer.MAX_VALUE;
 
-        for (int i=1;i<=N;i++){
-            for (int j=1;j<=N;j++){
-                int k=sc.nextInt();
-
-                if (k==1) homes.add(new int[]{i,j});
-                else if (k==2) chicken.add(new int[]{i,j});
-            }
-        }
-        visited=new boolean[chicken.size()];
-
-        go(0,0);
-        System.out.println(ans);
-    }
-
-    public static void go(int idx,int cnt){
-        if (cnt==M){
-            int temp=0;
-            for (int[] home:homes){
-                int ret=Integer.MAX_VALUE;
-                for (int i=0;i< chicken.size();i++){
-                    if (visited[i]){
-                        int dist=Math.abs(home[0]-chicken.get(i)[0])+Math.abs(home[1]-chicken.get(i)[1]);
-
-                        //한 가정에서 최소거리를 구한다
-                        ret=Math.min(ret,dist);
-                    }
+        for (int i=0;i<N;i++){
+            String[] raw=br.readLine().split(" ");
+            for (int j=0;j<raw.length;j++){
+                if (raw[j].equals("1")){
+                    house.add(new int[]{i,j});
+                }else if (raw[j].equals("2")){
+                    chicken.add(new int[]{i,j});
                 }
-                temp+=ret;
             }
-            ans=Math.min(ans,temp);
-            return;
         }
-        for (int i=idx;i< chicken.size();i++){
-            visited[i]=true;
-            go(i+1,cnt+1);
-            visited[i]=false;
-        }
+        //치킨 조합을 구한다
+        go(0,0);
+        System.out.println(ret);
     }
 
+    static void go(int cur,int depth){
+
+        if (depth==M){
+            int cmp=0;
+            for (int j=0;j<house.size();j++){
+                int[] curH=house.get(j);
+                int x=curH[0],y=curH[1];
+                int minDist=Integer.MAX_VALUE;
+                for (int i=0;i<M;i++){
+                    int cX=ar[i].x,cY=ar[i].y;
+                    minDist=Math.min(Math.abs(x-cX)+Math.abs(y-cY),minDist);
+                }
+                cmp+=minDist;
+            }
+            ret=Math.min(ret,cmp);
+        }
+
+        for (int i=cur;i<chicken.size();i++){
+            int[] curC=chicken.get(i);
+            int x=curC[0],y=curC[1];
+
+            if (!visited[x][y]){
+                visited[x][y]=true;
+                ar[depth]=new Pair(x,y);
+                go(i+1,depth+1);
+                visited[x][y]=false;
+            }
+        }
+
+    }
 }

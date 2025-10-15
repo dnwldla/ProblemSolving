fd87ce64025181f4365f6110a5978d5d070a3c43
dnwldla <94666277+dnwldla@users.noreply.github.com>
2025-10-15 11:16:33 +0900

    [Gold II] Title: 원판 돌리기, Time: 148 ms, Memory: 15604 KB -BaekjoonHub

--- Java/백준/Gold/17822. 원판 돌리기/원판 돌리기.java
+++ Java/백준/Gold/17822. 원판 돌리기/원판 돌리기.java
@@ -1,159 +1,134 @@
+import java.io.*;
 import java.util.*;
 
-//(1,1) 부터 시작
-public class Main{
-    //M개의 정수
-    static int N,M,T,cnt;
-    static int[][] ar;
-    static int[] dx={0,0,1,-1};
-    static int[] dy={-1,1,0,0};
-    static boolean flag=false;
+public class Main {
+    static int n, m, T;
+    static int[][] a;
     static boolean[][] visited;
-    static List<int[]> change=new ArrayList<>();
-    public static void main(String[] args) {
-        //입력
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt(); M=sc.nextInt(); T=sc.nextInt();
-        cnt=N*M;
+    static int ret = 0;
+    static final int[] dy = {-1, 0, 1, 0};
+    static final int[] dx = {0, 1, 0, -1};
+    static boolean noAdjEqual = true;
 
-        //회전 입력
-        ar=new int[N+1][M+1];
-        for (int i=1;i<=N;i++){
-            for (int j=1;j<=M;j++){
-                ar[i][j]=sc.nextInt();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        T = Integer.parseInt(st.nextToken());
+
+        a = new int[n][m];
+        visited = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                a[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        //T개 줄에 x,d,k
-        for (int i=0;i<T;i++){
-            int x,d,k;
-            x=sc.nextInt(); d=sc.nextInt(); k=sc.nextInt();
-            visited=new boolean[N+1][M+1];
+        for (int t = 0; t < T; t++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
 
-            //x의 배수를 d방향으로 k칸 회전 ->N까지 가능
-            int tmp=1;
-            //회전한다
-            while (true){
-                if (x*tmp>N) break;
-                rotate(x*tmp,d,k%M);
-                tmp++;
-//                for (int p=1;p<=N;p++){
-//                    for (int q=1;q<=M;q++){
-//                        System.out.printf(ar[p][q]+" ");
-//                    }
-//                    System.out.println();
-//                }
-//                System.out.println("rotate end");
-                //인접하면서 같은 수가 있는걸 찾느다
-
-            }
-            //회전 끝
-//            System.out.println("rotate end range");
-//            for (int p=1;p<=N;p++){
-//                for (int q=1;q<=M;q++){
-//                    System.out.printf(ar[p][q]+" ");
-//                }
-//                System.out.println();
-//            }
-//            System.out.println("one rotate end");
-            flag=false;
-            //인접한 점 체크
-            for (int p=1;p<=N;p++){
-                for (int q=1;q<=M;q++){
-                    if (ar[p][q]==0 || visited[p][q]) continue;
-                    dfs(p,q);
-                    for (int[] c:change) ar[c[0]][c[1]]=0;
-                    change=new ArrayList<>();
-
-                }
+            for (int row = x - 1; row < n; row += x) {
+                rotateRow(row, d, k);
             }
 
-            if (flag==false){
-                double avg=getAvg();
-                update(avg);
+            if (findAdj()) {
+                setAverage();
             }
-//            System.out.println("cycle end start");
-//           // 한 사이클이 끝난 후 후 원판 상태
-//            for (int p=1;p<=N;p++){
-//                for (int q=1;q<=M;q++){
-//                    System.out.printf(ar[p][q]+" ");
-//                }
-//                System.out.println();
-//            }
-//            System.out.println("cycle end end");
         }
-        int ret=0;
-        for (int i=1;i<=N;i++){
-            for (int j=1;j<=M;j++){
-                ret+=ar[i][j];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                ret += a[i][j];
             }
         }
         System.out.println(ret);
-
-
     }
 
-    static void update(double avg){
-        for (int i=1;i<=N;i++){
-            for (int j=1;j<=M;j++){
-                if (ar[i][j]==0) continue;
-                else if (avg<(double)ar[i][j]) ar[i][j]-=1;
-                else if (avg>(double)ar[i][j]) ar[i][j]+=1;
+    static void rotateRow(int row, int dir, int k) {
+        k %= m;
+        if (k == 0) return;
+
+        int[] v = Arrays.copyOf(a[row], m);
+
+        if (dir == 1) {
+            int[] tmp = new int[m];
+            System.arraycopy(v, k, tmp, 0, m - k);
+            System.arraycopy(v, 0, tmp, m - k, k);
+            a[row] = tmp;
+        } else {
+            int left = (m - k) % m;
+            int[] tmp = new int[m];
+            System.arraycopy(v, left, tmp, 0, m - left);
+            System.arraycopy(v, 0, tmp, m - left, left);
+            a[row] = tmp;
+        }
+    }
+
+    static boolean findAdj() {
+        noAdjEqual = true;
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(visited[i], false);
+        }
+
+        for (int y = 0; y < n; y++) {
+            for (int x = 0; x < m; x++) {
+                if (a[y][x] == 0) continue;
+                if (visited[y][x]) continue;
+                dfs(y, x);
+            }
+        }
+
+        for (int y = 0; y < n; y++) {
+            for (int x = 0; x < m; x++) {
+                if (visited[y][x]) a[y][x] = 0;
+            }
+        }
+        return noAdjEqual;
+    }
+
+    static void dfs(int y, int x) {
+        for (int dir = 0; dir < 4; dir++) {
+            int ny = y + dy[dir];
+            int nx = (x + dx[dir] + m) % m;
+            if (ny < 0 || ny >= n) continue;
+            if (visited[ny][nx]) continue;
+            if (a[y][x] != 0 && a[y][x] == a[ny][nx]) {
+                visited[y][x] = true;
+                visited[ny][nx] = true;
+                noAdjEqual = false;
+                dfs(ny, nx);
             }
         }
     }
 
-    static double getAvg(){
-        double tot=0;
-        int cnt=0;
-        for (int i=1;i<=N;i++){
-            for (int j=1;j<=M;j++){
-                if (ar[i][j]!=0) cnt++;
-                tot+=ar[i][j];
+    static void setAverage() {
+        long sum = 0;
+        int cnt = 0;
+        for (int y = 0; y < n; y++) {
+            for (int x = 0; x < m; x++) {
+                if (a[y][x] == 0) continue;
+                sum += a[y][x];
+                cnt++;
             }
         }
-        return tot/(double)cnt;
-    }
+        if (cnt == 0) return;
 
-    static void dfs(int x,int y){
-        //baseCase
-        if (ar[x][y]==0) return;
-        for (int i=0;i<4;i++){
-            int nx=x+dx[i],ny=y+dy[i];
-            //열 보정
-            if (ny==0) ny=M; if (ny==M+1) ny=1;
-            if (nx<=0 || nx>N) continue;
-            if (ar[x][y]==ar[nx][ny] && ar[x][y]!=0 && !visited[nx][ny]){
-                if (!visited[x][y]) change.add(new int[]{x,y});
-                change.add(new int[]{nx,ny});
-                visited[nx][ny]=true; if (!visited[x][y]) visited[x][y]=true;
+        double avg = (double) sum / (double) cnt;
 
-                flag=true;
-                dfs(nx,ny);
+        for (int y = 0; y < n; y++) {
+            for (int x = 0; x < m; x++) {
+                if (a[y][x] == 0) continue;
+                if ((double) a[y][x] > avg) a[y][x]--;
+                else if ((double) a[y][x] < avg) a[y][x]++;
             }
         }
-    }
-    //
-    static void rotate(int x,int d,int k){
-       int[] tmp=new int[M+1];
-       int idx=0;
-        if (d==0){//시계방향 회전
-            for (int i=1;i<=M;i++){
-                idx=(i+k)%M;
-                if (idx==0) idx=M;
-                tmp[idx]=ar[x][i];
-            }
-        }else{
-            for (int i=1;i<=M;i++){
-                idx=(i+M-k)%M;
-                if (idx==0) idx=M;
-                tmp[idx]=ar[x][i];
-            }
-        }
-        //원복
-        for (int i=1;i<=M;i++){
-            ar[x][i]=tmp[i];
-        }
-
     }
 }
